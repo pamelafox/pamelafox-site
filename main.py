4b68cd406a4fe55b9c41a55d0c78058a1ab2159e
Pamela Fox <pamela.fox@gmail.com>
2011-02-05 07:42:27 +1100

    Changing caching, adding query to reading list

--- main.py
+++ main.py
@@ -11,38 +11,58 @@ from google.appengine.ext.webapp import template
 from google.appengine.api import memcache
 from google.appengine.api import urlfetch
 from django.utils import simplejson
+  
     
-def getpage(filename, values={}):
-  page = memcache.get(filename)
-  #memcache.delete(filename)
-  #page = None
-  if page:
-    return page
-  else:
-    path = os.path.join(os.path.dirname(__file__), filename)
-    page = template.render(path, values)
-    memcache.set(filename, page, 60*30)
-    return page
-    
-    
-class HomePage(webapp.RequestHandler):
+class BaseRequestHandler(webapp.RequestHandler):
+
   def get(self):
+    page = memcache.get(self.get_cachename())
+    if not page:
+      path = os.path.join(os.path.dirname(__file__), self.get_filename())
+      page = template.render(path, self.get_values())
+      memcache.set(self.get_cachename(), page, 60*30)
+    self.response.out.write(page)
+    
+
+class HomePage(BaseRequestHandler):
+
+  def get_filename(self):
+    return 'index.html'
+
+  def get_cachename(self):
+    return 'index'
+
+  def get_values(self):
     pic = {}
     url = 'https://api.dailybooth.com/v1/users/254325/pictures.json?limit=1'
     result = urlfetch.fetch(url)
     if result.status_code == 200:
       json = simplejson.loads(result.content)
       pic = json[0]
-    self.response.out.write(getpage('index.html', {'pic': pic}))
+    return {'pic': pic}
+
     
-class ReadingList(webapp.RequestHandler):
-  def get(self):
+class ReadingList(BaseRequestHandler):
+
+  def get_filename(self):
+    return 'readinglist.html'
+
+  def get_cachename(self):
+     return 'readinglist' + self.request.get('q', '')
+
+  def get_values(self):
     books = []
     url = 'https://spreadsheets.google.com/feeds/list/0Ah0xU81penP1dFNLWk5YMW41dkcwa1JNQXk3YUJoOXc/od6/public/values?sq=include%3Dyes&alt=json'
+    if self.request.get('q'):
+      url += '&q=' + self.request.get('q')
     result = urlfetch.fetch(url)
     if result.status_code == 200:
       json = simplejson.loads(result.content)
-      entries = json["feed"]["entry"]
+      feed = json['feed']
+      if 'entry' in feed:
+        entries = feed['entry']
+      else:
+        entries = []
       for entry in entries:
         fields = ['title', 'author', 'asin', 'review', 'rating', 'thumbnail']
         book_info = {}
@@ -50,8 +70,7 @@ class ReadingList(webapp.RequestHandler):
           if entry['gsx$' + field]:
             book_info[field] = entry['gsx$' + field]['$t']
         books.append(book_info)
-      
-    self.response.out.write(getpage('readinglist.html', {'books': books}))
+    return {'books': books}
     
     
 application = webapp.WSGIApplication(
