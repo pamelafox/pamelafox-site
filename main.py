3a7f80dc89c8f06761da08e066c553684e508cd4
Pamela Fox <pamela.fox@gmail.com>
2011-02-06 22:45:47 +1100

    Adding talks and tags

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 """
  
 import os
+import logging
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
@@ -23,6 +24,37 @@ class BaseRequestHandler(webapp.RequestHandler):
       memcache.set(self.get_cachename(), page, 60*30)
     self.response.out.write(page)
     
+  def get_worksheet_data(self, worksheet_id, fields, extra_query=None):
+    url = 'https://spreadsheets.google.com/feeds/list/0Ah0xU81penP1dFNLWk5YMW41dkcwa1JNQXk3YUJoOXc/' + worksheet_id + '/public/values?sq=include%3Dyes&alt=json'
+    if extra_query:
+      url += extra_query
+    filter = None
+    if self.request.get('q'):
+      filter = self.request.get('q')
+      url += '&q=' + filter
+    result = urlfetch.fetch(url)
+    rows = []
+    tags = []
+    if result.status_code == 200:
+      json = simplejson.loads(result.content)
+      feed = json['feed']
+      entries = []
+      if 'entry' in feed:
+        entries = feed['entry']
+      for entry in entries:
+        row_info = {}
+        for field in fields:
+          if entry['gsx$' + field]:
+            row_info[field] = entry['gsx$' + field]['$t']
+            if field == 'tags':
+              row_tags = row_info['tags'].split(',')
+              for tag in row_tags:
+                tag = tag.strip()
+                if tag not in tags and len(tag) > 0:
+	              tags.append(tag)
+        rows.append(row_info)
+    return rows, tags, filter
+
 
 class HomePage(BaseRequestHandler):
 
@@ -39,7 +71,7 @@ class HomePage(BaseRequestHandler):
     if result.status_code == 200:
       json = simplejson.loads(result.content)
       pic = json[0]
-    return {'pic': pic}
+    return {'pic': pic, 'title': 'pamela fox'}
 
     
 class ReadingList(BaseRequestHandler):
@@ -51,31 +83,34 @@ class ReadingList(BaseRequestHandler):
      return 'readinglist' + self.request.get('q', '')
 
   def get_values(self):
-    books = []
-    url = 'https://spreadsheets.google.com/feeds/list/0Ah0xU81penP1dFNLWk5YMW41dkcwa1JNQXk3YUJoOXc/od6/public/values?sq=include%3Dyes&alt=json'
-    if self.request.get('q'):
-      url += '&q=' + self.request.get('q')
-    result = urlfetch.fetch(url)
-    if result.status_code == 200:
-      json = simplejson.loads(result.content)
-      feed = json['feed']
-      if 'entry' in feed:
-        entries = feed['entry']
-      else:
-        entries = []
-      for entry in entries:
-        fields = ['title', 'author', 'asin', 'review', 'rating', 'thumbnail']
-        book_info = {}
-        for field in fields:
-          if entry['gsx$' + field]:
-            book_info[field] = entry['gsx$' + field]['$t']
-        books.append(book_info)
-    return {'books': books}
+    fields = ['title', 'author', 'asin', 'review', 'rating', 'thumbnail', 'tags']
+    books, tags, filter = self.get_worksheet_data('od6', fields)
+    title = 'pamela fox\'s reading list'
+    if filter:
+	  title += ' :: ' + filter
+    return {'books': books, 'tags': tags, 'filter': filter, 'title': title}
     
+class Talks(BaseRequestHandler):
+
+  def get_filename(self):
+    return 'talks.html'
     
+  def get_cachename(self):
+    return 'talks' + self.request.get('q', '')
+
+  def get_values(self):
+    fields = ['title', 'date', 'description', 'thumbnail', 'slides', 'video', 'tags', 'location']
+    talks, tags, filter = self.get_worksheet_data('od7', fields, '&orderby=column:date&reverse=true')
+    title = 'pamela fox\'s talks'
+    if filter:
+	  title += ' :: ' + filter
+    return {'talks': talks, 'tags': tags, 'filter': filter, 'title': title}
+
+
 application = webapp.WSGIApplication(
                                      [('/', HomePage),
-                                      ('/readinglist', ReadingList)],
+                                      ('/readinglist', ReadingList),
+                                      ('/talks', Talks)],
                                      debug=True)
 
 def main():
