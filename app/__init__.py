cb4c284afa3465c9474f984a5a82adca484bc4a6
Pamela Fox <pamela.fox@gmail.com>
2022-06-23 11:27:20 -0700

    Black formatting

--- app/__init__.py
+++ app/__init__.py
@@ -9,52 +9,67 @@ from .datasources import get_worksheet_data, get_blogger_data
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 @cache_control(20)
 def home_page():
-  values = {'title': 'pamela fox'}
-  return render_template('index.html', **values)
-    
-@app.route('/readinglist')
+    values = {"title": "pamela fox"}
+    return render_template("index.html", **values)
+
+
+@app.route("/readinglist")
 @cache_control(60)
 def readinginglist():
-  values = {'title': 'pamela fox\'s reading list'}
-  return render_template('readinglist.html', **values)
+    values = {"title": "pamela fox's reading list"}
+    return render_template("readinglist.html", **values)
 
-@app.route('/talks')
+
+@app.route("/talks")
 @cache_control(10)
 def talks():
-  fields = ['title', 'date', 'description', 'thumbnail', 'slides', 'video', 'tags', 'location']
-  talks, tags, filter = get_worksheet_data('Talks')
-  title = 'pamela fox\'s talks'
-  if filter:
-    title += ' :: ' + filter
-  values = {'talks': talks, 'tags': tags, 'filter': filter, 'title': title}
-  return render_template('talks.html', **values) 
+    fields = [
+        "title",
+        "date",
+        "description",
+        "thumbnail",
+        "slides",
+        "video",
+        "tags",
+        "location",
+    ]
+    talks, tags, filter = get_worksheet_data("Talks")
+    title = "pamela fox's talks"
+    if filter:
+        title += " :: " + filter
+    values = {"talks": talks, "tags": tags, "filter": filter, "title": title}
+    return render_template("talks.html", **values)
 
-@app.route('/projects') 
+
+@app.route("/projects")
 @cache_control(10)
 def projects():
-    fields = ['title', 'date', 'description', 'homepage', 'source', 'thumbnail']
-    projects, tags, filter = get_worksheet_data('Projects', fields)
-    title = 'pamela fox\'s projects'
-    values = {'projects': projects, 'tags': tags, 'filter': filter, 'title': title}
-    return render_template('projects.html', **values)
+    fields = ["title", "date", "description", "homepage", "source", "thumbnail"]
+    projects, tags, filter = get_worksheet_data("Projects", fields)
+    title = "pamela fox's projects"
+    values = {"projects": projects, "tags": tags, "filter": filter, "title": title}
+    return render_template("projects.html", **values)
 
-@app.route('/interviews')
+
+@app.route("/interviews")
 @cache_control(10)
 def interviews():
-  fields = ['title', 'url']
-  interviews, tags, filter = get_worksheet_data('Interviews', fields)
-  title = 'pamela fox\'s interviews'
-  values = {'interviews': interviews, 'title': title}
-  return render_template('interviews.html', **values)
+    fields = ["title", "url"]
+    interviews, tags, filter = get_worksheet_data("Interviews", fields)
+    title = "pamela fox's interviews"
+    values = {"interviews": interviews, "title": title}
+    return render_template("interviews.html", **values)
 
-@app.route('/blogposts')
+
+@app.route("/blogposts")
 @cache_control(15)
 def blogposts():
-  tag = request.args.get('tag', None)
-  posts, tags, tag = get_blogger_data(tag)
-  title = 'pamela fox\'s blog posts'
-  values = {'tags': tags, 'posts': posts, 'title': title}
-  return render_template('blogposts.html', **values)
+    tag = request.args.get("tag", None)
+    posts, tags, tag = get_blogger_data(tag)
+    title = "pamela fox's blog posts"
+    values = {"tags": tags, "posts": posts, "title": title}
+    return render_template("blogposts.html", **values)
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -2,16 +2,19 @@ import pytest
 
 from app import app
 
+
 @pytest.fixture()
 def client():
     return app.test_client()
 
+
 def test_homepage(client):
-  response = client.get('/')
-  assert response.status_code == 200
-  assert b'I am a human' in response.data
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"I am a human" in response.data
+
 
 def test_homepage_cache(client):
-  response = client.get('/')
-  assert 'Cache-Control' in response.headers
-  assert 'max-age=4320000' in response.headers['Cache-Control']
+    response = client.get("/")
+    assert "Cache-Control" in response.headers
+    assert "max-age=4320000" in response.headers["Cache-Control"]
