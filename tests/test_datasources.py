84b49368a5629cde2736b0a23d7618a2bdf79f5a
Pamela Fox <pamela.fox@gmail.com>
2022-06-24 11:09:45 -0700

    Black

--- tests/test_datasources.py
+++ tests/test_datasources.py
@@ -5,6 +5,7 @@ import unittest.mock
 
 from app.datasources import get_worksheet_data, get_blogger_data
 
+
 def test_get_worksheet_data():
     fake_data = {
         "range": "Projects!A1:T102",
@@ -40,48 +41,58 @@ def test_get_worksheet_data():
 
 
 fake_blogger_data = {
-        "feed": {
-            "category": [{"term": "khanacademy"}, {"term": "eatdifferent"}, {"term": "musicvideo"}],
-            "entry": [
-                {
-                    "id": {
-                        "$t": "tag:blogger.com,1999:blog-8501278254137514883.post-7028526666522021237"
+    "feed": {
+        "category": [{"term": "khanacademy"}, {"term": "eatdifferent"}, {"term": "musicvideo"}],
+        "entry": [
+            {
+                "id": {
+                    "$t": "tag:blogger.com,1999:blog-8501278254137514883.post-7028526666522021237"
+                },
+                "published": {"$t": "2022-06-07T15:26:00.005-07:00"},
+                "updated": {"$t": "2022-06-07T15:30:18.333-07:00"},
+                "title": {"type": "text", "$t": "How accessibility helps a nursing mother"},
+                "category": [{"scheme": "http://www.blogger.com/atom/ns#", "term": "a11y"}],
+                "link": [
+                    {
+                        "rel": "self",
+                        "type": "application/atom+xml",
+                        "href": "https://www.blogger.com/feeds/8501278254137514883/posts/default/7028526666522021237",
                     },
-                    "published": {"$t": "2022-06-07T15:26:00.005-07:00"},
-                    "updated": {"$t": "2022-06-07T15:30:18.333-07:00"},
-                    "title": {"type": "text", "$t": "How accessibility helps a nursing mother"},
-                    "category":[{"scheme":"http://www.blogger.com/atom/ns#","term":"a11y"}],
-                    "link": [
-                        {
-                            "rel": "self",
-                            "type": "application/atom+xml",
-                            "href": "https://www.blogger.com/feeds/8501278254137514883/posts/default/7028526666522021237",
-                        },
-                        {
-                            "rel": "alternate",
-                            "type": "text/html",
-                            "href": "http://blog.pamelafox.org/2022/06/how-accessibility-helps-nursing-mother.html",
-                            "title": "How accessibility helps a nursing mother",
-                        },
-                    ],
-                }
-            ],
-        }
+                    {
+                        "rel": "alternate",
+                        "type": "text/html",
+                        "href": "http://blog.pamelafox.org/2022/06/how-accessibility-helps-nursing-mother.html",
+                        "title": "How accessibility helps a nursing mother",
+                    },
+                ],
+            }
+        ],
     }
+}
+
 
 def test_get_blogger_data():
     data = io.StringIO(json.dumps(fake_blogger_data))
     data.status = 200
     with unittest.mock.patch.object(urllib.request, "urlopen", return_value=data):
         rows, tags, tag = get_blogger_data()
-        assert rows == [{'link': 'http://blog.pamelafox.org/2022/06/how-accessibility-helps-nursing-mother.html', 'title': 'How accessibility helps a nursing mother'}]
-        assert tags == ['khanacademy', 'eatdifferent', 'musicvideo']
+        assert rows == [
+            {
+                "link": "http://blog.pamelafox.org/2022/06/how-accessibility-helps-nursing-mother.html",
+                "title": "How accessibility helps a nursing mother",
+            }
+        ]
+        assert tags == ["khanacademy", "eatdifferent", "musicvideo"]
         assert tag == None
+
 
 def test_get_blogger_data_with_tag():
     data = io.StringIO(json.dumps(fake_blogger_data))
     data.status = 200
     with unittest.mock.patch.object(urllib.request, "urlopen", return_value=data) as mocked:
-        rows, tags, tag = get_blogger_data('a11y')
-        assert mocked.call_args.args[0] == 'http://www.blogger.com/feeds/8501278254137514883/posts/default?max-results=150&alt=json&category=a11y'
-        assert tag == 'a11y'
+        rows, tags, tag = get_blogger_data("a11y")
+        assert (
+            mocked.call_args.args[0]
+            == "http://www.blogger.com/feeds/8501278254137514883/posts/default?max-results=150&alt=json&category=a11y"
+        )
+        assert tag == "a11y"
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,19 +10,23 @@ from app import datasources
 def client():
     return app.test_client()
 
+
 @pytest.fixture()
 def fake_worksheet_data():
     return unittest.mock.patch.object(datasources, "get_worksheet_data", return_value=[])
+
 
 def test_homepage(client):
     response = client.get("/")
     assert response.status_code == 200
     assert b"I am a human" in response.data
 
+
 def test_homepage_cache(client):
     response = client.get("/")
     assert "Cache-Control" in response.headers
     assert "max-age=4320000" in response.headers["Cache-Control"]
+
 
 def test_projects(client, fake_worksheet_data):
     with fake_worksheet_data:
@@ -30,11 +34,13 @@ def test_projects(client, fake_worksheet_data):
         assert response.status_code == 200
         assert b"projects" in response.data
 
+
 def test_talks(client, fake_worksheet_data):
     with fake_worksheet_data:
         response = client.get("/talks")
         assert response.status_code == 200
         assert b"talks" in response.data
+
 
 def test_interviews(client, fake_worksheet_data):
     with fake_worksheet_data:
@@ -42,22 +48,25 @@ def test_interviews(client, fake_worksheet_data):
         assert response.status_code == 200
         assert b"interviews" in response.data
 
+
 def test_readinglist(client):
-        response = client.get("/readinglist")
-        assert response.status_code == 200
-        assert b"booklist" in response.data
+    response = client.get("/readinglist")
+    assert response.status_code == 200
+    assert b"booklist" in response.data
+
 
 def test_blogposts(client):
-    with unittest.mock.patch('app.get_blogger_data') as mocked:
+    with unittest.mock.patch("app.get_blogger_data") as mocked:
         mocked.return_value = [], [], None
         response = client.get("/blogposts")
         assert response.status_code == 200
         assert b"blog posts" in response.data
 
+
 def test_blogposts_tag(client):
-    with unittest.mock.patch('app.get_blogger_data') as mocked:
-        mocked.return_value = [], [], 'a11y'
+    with unittest.mock.patch("app.get_blogger_data") as mocked:
+        mocked.return_value = [], [], "a11y"
         response = client.get("/blogposts?tag=a11y")
-        assert mocked.call_args.args[0] == 'a11y'
+        assert mocked.call_args.args[0] == "a11y"
         assert response.status_code == 200
         assert b"blog posts" in response.data
