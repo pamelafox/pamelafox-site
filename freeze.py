893f7e084ad03b5ca0edf81ec805acb0acffbfc8
Pamela Fox <pamela.fox@gmail.com>
2023-02-11 16:19:28 -0800

    ported to frozen flask and swa

---
+++ freeze.py
@@ -0,0 +1,7 @@
+from flask_frozen import Freezer
+from src import app
+
+freezer = Freezer(app)
+
+if __name__ == '__main__':
+    freezer.freeze()
--- src/__init__.py
+++ src/__init__.py
@@ -2,30 +2,31 @@ import os
 import json
 import urllib.request
 
-from flask import Flask, render_template, request
+from flask import Flask, make_response, render_template, request
 
-from .decorators import cache_control
 from .datasources import get_worksheet_data, get_blogger_data
 
 app = Flask(__name__)
 
+def render_html_template(filename, **context):
+    resp = make_response(render_template(filename, **context))
+    resp.headers['Content-type'] = 'text/html; charset=utf-8'
+    return resp
+
 
 @app.route("/")
-@cache_control(20)
 def home_page():
     values = {"title": "pamela fox"}
-    return render_template("index.html", **values)
+    return render_html_template("index.html", **values)
 
 
-@app.route("/readinglist")
-@cache_control(60)
+@app.route("/readinglist.html")
 def readinginglist():
     values = {"title": "pamela fox's reading list"}
-    return render_template("readinglist.html", **values)
+    return render_html_template("readinglist.html", **values)
 
 
-@app.route("/talks")
-@cache_control(10)
+@app.route("/talks.html")
 def talks():
     fields = [
         "title",
@@ -40,32 +41,29 @@ def talks():
     talks = get_worksheet_data("Talks")
     title = "pamela fox's talks"
     values = {"talks": talks, "title": title}
-    return render_template("talks.html", **values)
+    return render_html_template("talks.html", **values)
 
 
-@app.route("/projects")
-@cache_control(10)
+@app.route("/projects.html")
 def projects():
     projects = get_worksheet_data("Projects")
     title = "pamela fox's projects"
     values = {"projects": projects, "title": title}
-    return render_template("projects.html", **values)
+    return render_html_template("projects.html", **values)
 
 
-@app.route("/interviews")
-@cache_control(10)
+@app.route("/interviews.html")
 def interviews():
     interviews = get_worksheet_data("Interviews")
     title = "pamela fox's interviews"
     values = {"interviews": interviews, "title": title}
-    return render_template("interviews.html", **values)
+    return render_html_template("interviews.html", **values)
 
 
-@app.route("/blogposts")
-@cache_control(15)
+@app.route("/blogposts.html")
 def blogposts():
     tag = request.args.get("tag", None)
     posts, tags, tag = get_blogger_data(tag)
     title = "pamela fox's blog posts"
     values = {"tags": tags, "posts": posts, "title": title}
-    return render_template("blogposts.html", **values)
+    return render_html_template("blogposts.html", **values)
