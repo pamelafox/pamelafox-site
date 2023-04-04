87e4c8ae4194eb55f96dfa652086a4e4a53d80b4
Pamela Fox <pamela.fox@gmail.com>
2023-04-04 04:17:55 +0000

    More in workflow

--- src/tests/axe_analysis.py
+++ src/tests/axe_analysis.py
@@ -13,5 +13,5 @@ def test_a11y(app, live_server, page: Page):
     # for route in app.url_map():
     page.goto(url_for("projects", _external=True))
     results = axe.run(page)
-    Axe.save_results(results, os.path.join(os.path.dirname(__file__), "axe_results.txt"))
+    Axe.save_results(results, os.path.join(os.path.dirname(__file__), "axe_results.json"))
     assert len(results["violations"]) == 0, Axe.report_violations(results)
