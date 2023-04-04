44378891e404e64ade58336baf7084c597da92a2
Pamela Fox <pamela.fox@gmail.com>
2023-04-04 13:41:01 -0700

    Fix a11y errors

--- src/tests/axe_analysis.py
+++ src/tests/axe_analysis.py
@@ -10,8 +10,16 @@ multiprocessing.set_start_method("fork")
 
 def test_a11y(app, live_server, page: Page):
     axe = Axe()
-    # for route in app.url_map():
-    page.goto(url_for("home_page", _external=True))
-    results = axe.run(page, {"resultTypes": ["violations"]})
-    Axe.save_results(results, os.path.join(os.path.dirname(__file__), "axe_results.json"), violations_only=True)
-    assert len(results["violations"]) == 0, Axe.report_violations(results)
+    violations_count = 0
+    violations_reports = ""
+    directory_path = os.path.join(os.path.dirname(__file__), "results")
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+    for route in ["home_page", "projects"]:
+        page.goto(url_for(route, _external=True))
+        results = axe.run(page, {"resultTypes": ["violations"]})
+        Axe.save_results(results, os.path.join(directory_path, f"axe_{route}.json"), violations_only=True)
+        # Track violations for report at the end
+        violations_count += len(results["violations"])
+        violations_reports += Axe.report_violations(results)
+    assert violations_count == 0, violations_reports
