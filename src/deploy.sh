b84e7e4267933b2e79409d82ee20476e77ed4d22
Pamela Fox <pamela.fox@gmail.com>
2023-04-14 16:58:52 +0000

    Fix deploy

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-python3 src/freeze.py
-FILE="src/flaskapp/build/talks/index.html"
+python3 freeze.py
+FILE="flaskapp/build/talks/index.html"
 echo "*** File - $FILE contents ***"
 head -n 40 $FILE
 exit 0
