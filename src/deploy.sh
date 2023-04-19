4f7e81941ac3c8bf1224c9ec3c1b33552a7562b9
Victor Vazquez <vhvb1989@gmail.com>
2023-04-19 02:26:29 +0000

    try auto

--- src/deploy.sh
+++ src/deploy.sh
@@ -6,5 +6,5 @@ python3 -m venv scripts/.venv
 echo 'Installing dependencies from "requirements.txt" into virtual environment'
 ./scripts/.venv/bin/python -m pip install -r requirements.txt
 
-echo 'Running "prepdocs.py"'
+echo 'Running "Python Freeze"'
 ./scripts/.venv/bin/python ./freeze.py
