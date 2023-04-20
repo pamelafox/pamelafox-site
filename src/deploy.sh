751da25c1b883f9b7ad6a9a263685335c1782fb0
Victor Vazquez <vhvb1989@gmail.com>
2023-04-20 19:47:25 +0000

    fix for swa-cli on ci build

--- src/deploy.sh
+++ src/deploy.sh
@@ -6,5 +6,11 @@ python3 -m venv scripts/.venv
 echo 'Installing dependencies from "requirements.txt" into virtual environment'
 ./scripts/.venv/bin/python -m pip install -r requirements.txt
 
-echo 'Running "prepdocs.py"'
+echo 'Running "python freeze"'
 ./scripts/.venv/bin/python ./freeze.py
+
+# staticwebapp.config.json needs to be manually copyed because the swa-cli
+# fails when it tries to do this copy when running within a build pipeline (Github and Azdo).
+# See: https://github.com/Azure/static-web-apps-cli/issues/688
+echo 'copying swa settings'
+cp staticwebapp.config.json flaskapp/build/staticwebapp.config.json
